0cc4263d5409f030f3348913e9e54c9c4bdcc751
Khyati Kakkar <kkakkar5k@gmail.com>
2025-01-12 19:46:47 +0530

    Add Initial Files

--- Time-Conversion/Time.java
+++ Time-Conversion/Time.java
@@ -1,1 +1,23 @@
+public class Time {
+    public static void main(String[] args){
 
+        System.out.println(" Time passed in seconds is " +TimeConversion(56789));
+    }
+    public static int TimeConversion( int hours, int rSeconds  ,int rMinutes){
+
+        System.out.println("Total Time passed is " +hours+ "hr " +rMinutes+ "min and " +rSeconds+ "sec");
+
+        return 0;
+    }
+    public static int TimeConversion( int seconds){
+
+        if( seconds< 0 ){
+            System.out.println("Invalid value for seconds");
+        } else {
+
+            return  TimeConversion( seconds / 3600,(seconds % 3600)% 60 ,(seconds % 3600) / 60 )  ;
+
+        } return 0;
+    }
+
+}

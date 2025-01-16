46a796d8d104071da6feb855d6ef076a2723e22e
Khyati Kakkar <kkakkar5k@gmail.com>
2025-01-16 14:18:39 +0530

    Add Initial Files

---
+++ For-Loop/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public class Main {
+        public static void main(String [] args) {
+
+            for (int counter = 1; counter <= 5; counter++) {
+                System.out.println(counter);
+            }
+
+            for (double interestRate = 2; interestRate <= 5; interestRate++) {
+
+                System.out.println("10000 at " +interestRate +"% interest = " + calculateInterest(10000, interestRate));
+            }
+        }
+        public static double calculateInterest( double amount, double interestRate){
+
+            return (amount * ( interestRate / 100));
+        }
+    }
+}

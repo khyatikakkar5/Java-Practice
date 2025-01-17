a40a2aac2d1812b5f65ec4c24028c3ba20094b77
Khyati Kakkar <kkakkar5k@gmail.com>
2025-01-17 22:50:34 +0530

    Update

---
+++ For-Loop/Mini.java
@@ -0,0 +1,16 @@
+public class Mini {
+    public static void main(String[] args){
+
+        for ( double i = 7.5; i <= 10.0 ; i += 0.25){
+            if ( i > 8.5){
+                break;
+            }
+            System.out.println(" 100 dollars at " +i+ "% rate = $" +calculateInterest(100, i));
+
+        }
+    }
+    public static double calculateInterest( double amount,double i){
+
+        return ( amount * ( i / 100));
+    }
+}

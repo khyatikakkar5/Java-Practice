bb37bd71a3af3f2ae84e47548a2225d8150dde1c
Khyati Kakkar <kkakkar5k@gmail.com>
2025-01-28 13:18:14 +0530

    Update

--- WhileAndDoWhileLoops/DoWhile.java
+++ WhileAndDoWhileLoops/DoWhile.java
@@ -1,5 +1,5 @@
 public class DoWhile {
-    public static void main(String[] args){
+    public static void main(String... args){
 
         for (int i = 1; i <= 5 ; i++){
             System.out.println(i);

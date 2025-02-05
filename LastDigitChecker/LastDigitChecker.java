7334bda21ff1fc375af1b567c08c97aaa1c5ab0e
Khyati Kakkar <kkakkar5k@gmail.com>
2025-02-05 15:54:31 +0530

    Update

---
+++ LastDigitChecker/LastDigitChecker.java
@@ -0,0 +1,21 @@
+public class LastDigitChecker {
+    public static void main(String[] args){
+        System.out.println(hasSameLastDigit(22,33,44));
+        System.out.println(hasSameLastDigit(22,32,44));
+    }
+    public static boolean hasSameLastDigit(int firstNumber, int secondNumber, int thirdNumber) {
+        if((10 <= firstNumber) && (10 <= secondNumber) && (10 <= thirdNumber) && (firstNumber <= 1000) && (secondNumber <= 1000) && (thirdNumber <= 1000)) {
+            int firstRight = firstNumber % 10;
+            int secondRight = secondNumber % 10;
+            int thirdRight = thirdNumber % 10;
+
+            if((firstRight == secondRight) || (firstRight == thirdRight) || (thirdRight == secondRight)){
+                return true;
+            } else{
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+}

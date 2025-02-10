2739da81aaeb7d86d3d01293dc4d5c8571225d6f
Khyati Kakkar <kkakkar5k@gmail.com>
2025-02-10 15:45:46 +0530

    Update

--- Digit-Sum-Challenge/FirstLastDigitSum.java
+++ Digit-Sum-Challenge/FirstLastDigitSum.java
@@ -2,11 +2,13 @@ public class FirstLastDigitSum {
     public static void main(String[] args) {
         System.out.println(sumFirstAndLastDigit(28784389));
     }
-    
+
     public static int sumFirstAndLastDigit(int number){
+        
         if(number < 0){
             return -1;
         }
+
         int lastDigit = number % 10;
         while(number >= 10 ){
 

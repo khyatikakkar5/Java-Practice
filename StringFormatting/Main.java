098d486d43447fd3a31abc7b9084499ca4ca3b5e
Khyati Kakkar <kkakkar5k@gmail.com>
2025-02-13 17:42:36 +0530

    Update

--- StringFormatting/Main.java
+++ StringFormatting/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
 
         String bulletIt = "Print a Bulleted List:\n" +
                 "\t\u2022 First Point\n" +

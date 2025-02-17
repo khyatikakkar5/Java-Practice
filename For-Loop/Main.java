b1f700bc5454f2ccbcfcfa96e1991d693ef9d170
Khyati Kakkar <kkakkar5k@gmail.com>
2025-02-17 09:24:08 +0530

    Update

--- For-Loop/Main.java
+++ For-Loop/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-        public static void main(String [] args) {
+        public static void main(String... args) {
 
             for (int counter = 1; counter <= 5; counter++) {
                 System.out.println(counter);

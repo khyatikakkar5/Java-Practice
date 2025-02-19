388650ec047522994a8b35e467b92e06ac3a561b
Khyati Kakkar <kkakkar5k@gmail.com>
2025-02-19 10:29:39 +0530

    Update

--- Reading-User-Input/MinMax.java
+++ Reading-User-Input/MinMax.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class MinMax {
-    public static void main(String[] args) {
+    public static void main(String... args) {
 
         Scanner scanner = new Scanner(System.in);
 

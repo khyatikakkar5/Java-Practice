767f46ed1d9c2a72eaca499758a3a55d2db7ea18
Khyati Kakkar <kkakkar5k@gmail.com>
2025-02-19 10:31:39 +0530

    Update

--- Greatest-Commmon-Divisor/GreatestCommonDivisor.java
+++ Greatest-Commmon-Divisor/GreatestCommonDivisor.java
@@ -1,5 +1,5 @@
 public class GreatestCommonDivisor {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         System.out.println(getGreatestCommonDivisor(12,30));
     }
     

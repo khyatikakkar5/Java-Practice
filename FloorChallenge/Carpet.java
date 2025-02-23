c50395517fad0f4699e12375bd299af691700a4e
Khyati Kakkar <kkakkar5k@gmail.com>
2025-02-23 08:40:10 +0530

    Update

--- FloorChallenge/Carpet.java
+++ FloorChallenge/Carpet.java
@@ -1,1 +1,22 @@
+public class Carpet {
+    // write code here
+    private double cost;
 
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public Carpet(double cost) {
+        if (cost < 0) {
+            cost = 0;
+        }else {
+            this.cost = cost;
+        }
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+
+}

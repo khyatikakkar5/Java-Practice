7afbfb160ea043b838e785ceec93299139edcdf1
Khyati Kakkar <kkakkar5k@gmail.com>
2025-02-24 12:12:10 +0530

    Update

--- FloorChallenge/Floor.java
+++ FloorChallenge/Floor.java
@@ -1,1 +1,40 @@
+public class Floor {
+    // write code here
+    private double width;
+    private double length;
 
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public Floor(double width, double length) {
+        if ( width < 0){
+            width = 0;
+        }
+
+        if ( length < 0) {
+            length = 0;
+        }
+
+
+        this.width = width;
+        this.length = length;
+
+    }
+    public double getArea() {
+        return   width * length;
+    }
+
+}

4f7d1c65ba6dfcc18b4ade4ac65d284f953fc997
Khyati Kakkar <kkakkar5k@gmail.com>
2025-02-25 00:17:50 +0530

    Update

--- FloorChallenge/Main.java
+++ FloorChallenge/Main.java
@@ -1,1 +1,25 @@
+public class Main {
 
+
+
+    public static void main(String[] args) {
+
+        Floor floor = new Floor(45.5,50.6);
+        Carpet carpet = new Carpet(78.5);
+
+
+        double area = floor.getArea();
+        double cost = carpet.getCost();
+
+
+
+        System.out.println(floor.getArea());
+        System.out.println(carpet.getCost());
+        System.out.println(area * cost);
+
+
+
+    }
+
+
+}

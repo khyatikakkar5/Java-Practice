095e879d609578b13bcbdd2078f257a48a4c0390
Khyati Kakkar <kkakkar5k@gmail.com>
2025-02-25 00:18:26 +0530

    Update

--- FloorChallenge/Main.java
+++ FloorChallenge/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
         Floor floor = new Floor(45.5,50.6);
         Carpet carpet = new Carpet(78.5);

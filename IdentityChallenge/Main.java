a6e8f897d7e81cde2dc46df34f1e0ad63a84c063
Khyati Kakkar <kkakkar5k@gmail.com>
2025-03-01 22:09:13 +0530

    Update

--- IdentityChallenge/Main.java
+++ IdentityChallenge/Main.java
@@ -1,1 +1,24 @@
-
+public class Main {
+    public static void main(String[] args) {
+        Identity person = new Identity();
+        person.setFirstName("");   // firstName is set to empty string
+        person.setLastName("");    // lastName is set to empty string
+        person.setAge(10);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setFirstName("John");    // firstName is set to John
+        person.setAge(18);
+        System.out.println("fullName= " + person.getFullName());
+        System.out.println("teen= " + person.isTeen());
+        person.setLastName("Smith");    // lastName is set to Smith
+        System.out.println("fullName= " + person.getFullName());
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}

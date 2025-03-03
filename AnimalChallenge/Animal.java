b7c3e66be69a273abf78ffb081b7c99803d0d5b1
Khyati Kakkar <kkakkar5k@gmail.com>
2025-03-03 18:56:00 +0530

    Update

--- AnimalChallenge/Animal.java
+++ AnimalChallenge/Animal.java
@@ -9,6 +9,7 @@ public class Animal {
     }
 
     public Animal(String type, String size, double weight) {
+        
         this.type = type;
         this.size = size;
         this.weight = weight;

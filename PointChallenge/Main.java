f4da9605e5276a60896b7a880e1acb2048da5e97
Khyati Kakkar <kkakkar5k@gmail.com>
2025-03-04 21:51:14 +0530

    Update

--- PointChallenge/Main.java
+++ PointChallenge/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main(String... args) {
+    public static void main(String[] args) {
         
         Point first = new Point(6, 5);
         Point second = new Point(3, 1);

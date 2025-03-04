e055e076f99e1eb3a8dc22fd3cc9c26047960a13
Khyati Kakkar <kkakkar5k@gmail.com>
2025-03-04 21:44:48 +0530

    Update

--- AnimalChallenge/Fish.java
+++ AnimalChallenge/Fish.java
@@ -1,1 +1,50 @@
+public class Fish extends Animal {
 
+    private int gills;
+    private int fins;
+
+    public Fish(String type, double weight, int gills, int fins) {
+        super(type, "small", weight);
+        this.gills = gills;
+        this.fins = fins;
+    }
+
+    private void moveMuscles() {
+        System.out.print("muscles moving");
+    }
+
+    private void moveBackFin() {
+        System.out.print("Backfins moving");
+    }
+
+    public void move(String speed) {
+        super.move(speed);
+        moveMuscles();
+        if(speed == "fast") {
+            moveBackFin();
+        }
+        System.out.println();
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    @java.lang.Override
+    public String toString() {
+        return "Fish{" +
+                "gills=" + gills +
+                ", fins=" + fins +
+       
+       
+       
+       
+       
+                '}';
+    }
+}

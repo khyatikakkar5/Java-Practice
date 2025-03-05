4b4b5477b2b1cda9c606082ab4fafa0a95222e0d
Khyati Kakkar <kkakkar5k@gmail.com>
2025-03-05 20:46:20 +0530

    Update

--- AnimalChallenge/Main.java
+++ AnimalChallenge/Main.java
@@ -1,1 +1,46 @@
+public class Main {
 
+    public static void main(String[] args) {
+
+        Animal animal = new Animal("Generic Animal", "Huge",
+                400);
+        doAnimalStuff(animal,"slow");
+
+        Dog dog = new Dog();
+        doAnimalStuff(dog,"fast");
+
+        Dog labrador = new Dog("Labrador",30);
+        doAnimalStuff(labrador,"fast");
+        Dog retriever = new Dog("Retriever", 70,
+                
+                "Floppy","Pony");
+        doAnimalStuff(retriever,"slow");
+
+        Fish goldy = new Fish("Goldfish", 0.25,
+                2, 4);
+        
+        
+        doAnimalStuff(goldy,"fast");
+
+    }
+
+    public static void doAnimalStuff(Animal animal, String speed) {
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+        animal.makeNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("------");
+
+
+    }
+}

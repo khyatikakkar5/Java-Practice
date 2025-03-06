3f2638fc2d70900499b3ddd21926236a3994382d
Khyati Kakkar <kkakkar5k@gmail.com>
2025-03-07 00:02:43 +0530

    Update

--- AccountChallenge/Main.java
+++ AccountChallenge/Main.java
@@ -1,1 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
 
+        Account acc = new Account();
+        acc.withdrawFunds(100.0);
+        
+        
+        
+        okk
+        acc.depositFunds(250);
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}

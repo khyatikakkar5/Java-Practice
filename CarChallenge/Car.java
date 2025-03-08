6416e807780f4d27d29a77a0df9ca867867b9461
Khyati Kakkar <kkakkar5k@gmail.com>
2025-03-08 20:44:18 +0530

    Update

--- CarChallenge/Car.java
+++ CarChallenge/Car.java
@@ -43,7 +43,6 @@ public class Car {
     }
 
     public void setMake(String make) {
-
         if (make == null) make = "Unknown";
         String lowercaseMake = make.toLowerCase();
         switch(lowercaseMake) {

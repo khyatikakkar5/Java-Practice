d68c641e561e102849598cafd801a10a09ed3b9a
Khyati Kakkar <kkakkar5k@gmail.com>
2025-03-10 08:54:04 +0530

    Update

---
+++ ConstructorChallenge/Main.java
@@ -0,0 +1,22 @@
+package ConstructorChallenge;
+
+public class Main {
+
+    public static void main(String... args) {
+
+        Customer customer = new Customer("Khyati",1000,"Khyati@gmail.com");
+        System.out.println(customer.getName());
+        System.out.println(customer.getCreditLimit());
+        System.out.println(customer.getEmail());
+
+        Customer secondCustomer = new Customer();
+        System.out.println(secondCustomer.getName());
+        System.out.println(secondCustomer.getCreditLimit());
+        System.out.println(secondCustomer.getEmail());
+
+        Customer thirdCustomer = new Customer("Joe", "jeo@gmail.com");
+        System.out.println(thirdCustomer.getName());
+        System.out.println(thirdCustomer.getCreditLimit());
+        System.out.println(thirdCustomer.getEmail());
+    }
+}

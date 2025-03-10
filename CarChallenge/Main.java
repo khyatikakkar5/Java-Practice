0cba243807ec60d8d08592f7159c0e4aac4b0d57
Khyati Kakkar <kkakkar5k@gmail.com>
2025-03-10 08:47:51 +0530

    Update

---
+++ CarChallenge/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Car car = new Car();
+        
+        
+        car.setMake("Porsche");
+        System.out.println("make = " + car.getMake());
+        car.describeCar();
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        Car tar = new Car();
+        tar.setMake("tesla");
+        tar.setModel("Targa");
+        tar.describeCar();
+    }
+}
